543e92fd2ccc619535c959d6fedb90078970132c
Michael E. Karpeles <mekarpeles@gmail.com>
2012-06-24 20:40:23 -0700

    lazydb now has a bunch of utilities methods to make life a little lazier

--- lazydb/lazydb.py
+++ lazydb/lazydb.py
@@ -16,27 +16,59 @@ from contextlib import closing
 class Db(object):
 
     def __init__(self, database, dbpath=os.getcwd()):
-        self.name = database
-        self.file = "%s/%s" % (dbpath, database)
-        self.db = self.connect()
+        self._name = database
+        self._file = "%s/%s" % (dbpath, database)
+        self._db = self.connect()
 
     def connect(self):
-        return shelve.open(self.file, 'c')
+        return shelve.open(self._file, 'c')
 
+    def _write(self):
+        self._db.close()
+        self._db = self.connect()
+        
     def _destroy(self):
-        try:
-            return os.remove(self.file)
-        except:
-            return False
+        return os.remove(self._file)
+
+    def delete(self, key):
+        if self.has(key):
+            del self._db[key]
+            return True
 
     def get(self, key):
-        return self.db[key]
+        return self._db.get(key, [])
 
     def put(self, key, record):
-        self.db[key] = record
+        self._db[key] = record
+        self._write()
+        return record
 
-    def show_keys(self):
-        return self.db.keys()
+    def has(self, key):        
+        return key in self._db.keys()
+
+    def empty(self):
+        """boolean check to see if the database has any keys"""
+        return self.count()
+            
+    @property
+    def count(self):
+        """Counts the number of keys"""        
+        return len(self._db.keys())
+
+    @property
+    def keys(self):
+        """List keys in the db"""
+        return self._db.keys()
+
+    def records(self, key):
+        """length of the contents of a record"""
+        return len(self._db.get(key, []))
+
+    def append(self, key, record):
+        """should only work if obj @ key is type([])"""
+        records = self.get(key) + [record]
+        self.put(key, records)
+        self._write()
 
     @classmethod
     def create(cls, create):
@@ -44,10 +76,5 @@ class Db(object):
             db.close()
 
     @classmethod
-    def delete(self, database):
-        try:
-            return os.remove(self.file)
-        except:
-            return False        
-
-
+    def destroy(self, database):
+        return os.remove(self._file)
