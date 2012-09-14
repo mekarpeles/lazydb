0b1b6547cde9207bb3d83c7d2e4453302cf0f539
mek <michael.karpeles@gmail.com>
2012-09-14 14:14:20 -0700

    put and append are now working properly. Before append wasn't casting the existing record from (possibly) a str to a list before attempting append.

--- lazydb/lazydb.py
+++ lazydb/lazydb.py
@@ -15,17 +15,31 @@ from contextlib import closing
 
 class Db(object):
 
-    def __init__(self, database, dbpath=os.getcwd()):
-        self._name = database
-        self._file = "%s/%s" % (dbpath, database)
-        self._db = self.connect()
+    def __init__(self, db_name, db_path=os.getcwd()):
+        """Creates a connection to a flatfile database located in
+        db_path and named db_path using shelve wrappers. The database
+        file will be created/touched during init, if no such file
+        previously existed.
+        """
+        self._name = db_name
+        self._file = "{}/{}".format(db_path, db_name)
+        self._db = self.open()
 
-    def connect(self):
+    def open(self):
         return shelve.open(self._file, 'c')
 
+    def close(self):
+        return self._db.close()
+
+    @property
+    def connected(self):
+        """Test whether a connection is open"""
+        pass
+
     def _write(self):
+        """Crufty hack"""
         self._db.close()
-        self._db = self.connect()
+        self._db = self.open()
         
     def _destroy(self):
         return os.remove(self._file)
@@ -39,42 +53,60 @@ class Db(object):
         return self._db.get(key, [])
 
     def put(self, key, record):
+        """Adds a new key,val pair into the db. This will overwrite an
+        existing key,val pair. To add an additonal entry to an
+        existing record, use 'append'
+        """
         self._db[key] = record
         self._write()
-        return record
+        return self.get(key)
 
     def has(self, key):        
-        return key in self._db.keys()
+        """Determines whether a key exists within the database"""        
+        return key in self.keys()
 
-    def empty(self):
-        """boolean check to see if the database has any keys"""
-        return self.count()
-            
-    @property
-    def count(self):
-        """Counts the number of keys"""        
-        return len(self._db.keys())
+    def count(self, key):
+        """Returns the number of entries within a record"""
+        return len(self._db.get(key, []))
 
-    @property
+    def is_empty(self):
+        """boolean check to see if the database has any keys. Returns
+        0 if empty, else the number of keys in the db
+        """
+        return self.size()
+
+    def size(self):
+        """Returns the size of the database in number of entries"""
+        return len(self.keys())
+    
     def keys(self):
         """List keys in the db"""
         return self._db.keys()
 
-    def records(self, key):
-        """length of the contents of a record"""
-        return len(self._db.get(key, []))
+    def values(self):
+        """List all the values in the db"""
+        return [self.get(key) for key in self.keys()]
+
+    def items(self):
+        return [(key, self.get(key)) for key in self.keys()]
 
     def append(self, key, record):
         """should only work if obj @ key is type([])"""
-        records = self.get(key) + [record]
-        self.put(key, records)
+        records = self.get(key)
+        if not type(records) is list:
+            records = [records]        
+        records = self.put(key, records + [record])
+        assert(records == self.get(key))
         self._write()
+        return records
 
     @classmethod
-    def create(cls, create):
-        with closing(shelve.open(DB, 'c')) as db:
+    def create(cls, database):
+        """Used for creating arbitrary tmp databases"""
+        with closing(shelve.open(database, 'c')) as db:
             db.close()
 
     @classmethod
     def destroy(self, database):
-        return os.remove(self._file)
+        """Used for cleanup of arbitrary tmp databases"""
+        return os.remove(database)
--- lazydb/tests/test_lazydb.py
+++ lazydb/tests/test_lazydb.py
@@ -3,14 +3,20 @@ import uuid
 import os
 from lazydb.lazydb import Db
 
-DBNAME = uuid.uuid1()
+DB_NAME = uuid.uuid1()
+DB_PATH = "{}/tests/".format(os.getcwd())
 
 class TestLazyDB(unittest.TestCase):
 
-    def test_creation(self):
-        db = Db(DBNAME)
-        self.assertTrue(os.path.exists("%s/tests/%s" % (os.getcwd(), DBNAME)))
+    def setUp(self):
+        db = Db(db_name=DB_NAME, db_path=DB_PATH)
+        self.assertTrue(os.path.exists("{}{}".format(DB_PATH, DB_NAME)),
+                        "Database creation failed.")
         
-
+    def tearDown(self):
+        db = Db(db_name=DB_NAME, db_path=DB_PATH)
+        db._destroy()
+        self.assertTrue(os.path.exists("{}{}".format(DB_PATH, DB_NAME)),
+                        "Database deletion failed.")
         
         
