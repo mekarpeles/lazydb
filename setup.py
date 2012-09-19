d196ef71926bfee78493374783ea04eace70957b
mek <michael.karpeles@gmail.com>
2012-09-18 17:46:55 -0700

    CHANGES.txt
    v0.1.2 - Created setup.py + MANIFEST files for initial pypi dist build

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='LazyDB',
+    version='0.1.2',
+    author='mek',
+    author_email='michael.karpeles@gmail.com',
+    packages=['lazydb', 'lazydb.test'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/LazyDB',
+    license='LICENSE.txt',
+    long_description=open('README.txt').read(),
+)
