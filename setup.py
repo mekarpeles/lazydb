0913bed6e77161cd3744d95491860a4cca643823
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-01-19 04:15:04 -0800

    Bumping to v0.1.6, backwards compatibility for Python2.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='LazyDB',
-    version='0.1.4',
+    version='0.1.6',
     author='mek',
     author_email='michael.karpeles@gmail.com',
     packages=['lazydb', 'lazydb.test'],
