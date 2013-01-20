22626bc6e4758e7493f2342a22e8b4592183bbce
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-01-20 07:36:57 -0800

    Updated __init__.py to allow easier imports: from lazydb import Db

---
+++ lazydb/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+"""LazyDB is a basic wrapper around the Python shelve flatfile dbm module."""
+
+__version__ = "0.1.6"
+__author__ = [
+    "Mek <michael.karpeles@gmail.com>"
+]
+__license__ = "public domain"
+__contributors__ = "see AUTHORS"
+from lazydb import Db
