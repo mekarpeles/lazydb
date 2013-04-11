5369ec09ea7d362fb5ba8dae97456deb65a8d573
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-04-10 22:25:03 -0700

    switched to old style string formatting for py < 2.7

--- test/test_lazydb.py
+++ test/test_lazydb.py
@@ -17,28 +17,28 @@ class TestLazyDB(unittest.TestCase):
 
     def setUp(self):
         db = Db(DB_PATH)
-        self.assertTrue(os.path.exists("{}".format(DB_PATH)),
+        self.assertTrue(os.path.exists(DB_PATH),
                         "Database creation failed.")
         
     def test_put(self):
         db = Db(DB_PATH)
         db.put(TEST_KEY, TEST_VAL)
         db_val = db.get(TEST_KEY)
-        print "PUT: {}, GET: {}".format(TEST_ITEM, db_val)
+        print "PUT: %s, GET: %s" % (TEST_ITEM, db_val)
         self.assertTrue(db_val.__dict__ == TEST_VAL.__dict__,
-                        "Failed to 'put' TEST_ITEM {} " \
-                            "into {}".format(TEST_ITEM, DB_NAME))
+                        "Failed to 'put' TEST_ITEM %s " \
+                            "into %s" % (TEST_ITEM, DB_NAME))
 
     def test_append(self):
         db = Db(DB_PATH)
         db.put(TEST_KEY, TEST_VAL)
         db.append(TEST_KEY, TEST_VAL)
         db_val = db.get(TEST_KEY)
-        print "APPEND: {}, GET: {}".format(TEST_ITEM, db_val)
+        print "APPEND: %s, GET: %s" % (TEST_ITEM, db_val)
         self.assertTrue(map(lambda x: x.__class__, db_val) == \
                             map(lambda x: x.__class__, [TEST_VAL] * 2),
-                        "Failed to 'append' TEST_ITEM {} " \
-                            "into {}".format(TEST_ITEM, DB_NAME))
+                        "Failed to 'append' TEST_ITEM %s " \
+                            "into %s" % (TEST_ITEM, DB_NAME))
 
     def test_props(self):
         db = Db(DB_PATH)
@@ -48,4 +48,4 @@ class TestLazyDB(unittest.TestCase):
         db = Db(DB_PATH)
         db._destroy()
         self.assertTrue(not os.path.exists(DB_PATH),
-                        "Database failed to destroy tmp db {}".format(DB_PATH))
+                        "Database failed to destroy tmp db %s" % DB_PATH)
