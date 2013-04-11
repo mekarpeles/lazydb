3655bf2579ea22f73a60d1f5a028f36e58dd83df
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-04-10 23:09:05 -0700

    Requires 2.7 for now, removing support for 3.2

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 from sys import version
 
-if version < '2.7':
-    raise NotImplemented("LazyDb requires Python 2.7 or greater.")
+if version < '2.7' or version >= '3.0':
+    raise NotImplemented("LazyDb requires Python 2.7.*")
 
 setup(
     name='LazyDB',
